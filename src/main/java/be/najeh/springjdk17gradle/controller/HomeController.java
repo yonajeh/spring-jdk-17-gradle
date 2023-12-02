60f274296f58c17aea99164d3d03ac336d774ad8
Youssef <youssef.najeh@5thfloor.be>
2023-12-02 19:20:21 +0100

    add home endpoint

--- src/main/java/be/najeh/springjdk17gradle/controller/HomeController.java
+++ src/main/java/be/najeh/springjdk17gradle/controller/HomeController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping("/")
-    public String home(){
+    public String base(){
         return "SalamoAlikom";
+    }
+
+    @GetMapping("/home")
+    public String home(){
+        return "SalamoAlikom in home";
     }
 
 }
